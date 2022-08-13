7a20e4827ac17e7b1373ccf6615a1cc54cba5605
Marc Alba Cervero <marcalba80@gmail.com>
2022-08-14 00:47:40 +0200

    Element exception

--- src/main/java/servidor/controller/ChatController.java
+++ src/main/java/servidor/controller/ChatController.java
@@ -1,5 +1,7 @@
 package servidor.controller;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -29,36 +31,41 @@ public class ChatController {
     @MessageMapping("/chat")
     public void processMessage(@Payload /*@RequestBody*/ ChatMessage chatMessage){
         System.out.println("Chat");
-        Chat chat = new Chat();
-        chat.setUser1(userRepository.findByUsername(chatMessage.getUserFrom()).get());
-        chat.setUser2(userRepository.findByUsername(chatMessage.getUserTo()).get());
-        
-        if(!chatRepository.chatExists(chatMessage.getUserFrom(), chatMessage.getUserTo())){
-            System.out.println("Chat save");
-            chatRepository.save(chat);
-        }
-        if(userRepository.findByUsername(chatMessage.getUserTo()).get().isSignin()){
+        try{
+            Chat chat = new Chat();
+            chat.setUser1(userRepository.findByUsername(chatMessage.getUserFrom()).get());
+            chat.setUser2(userRepository.findByUsername(chatMessage.getUserTo()).get());
             
-            System.out.println("Chat send");
-            switch(chatMessage.getType()){
-                case ChatMessage.VALID_USER:
-                    simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserFrom(), "/queue/messages", 
-                    new ChatMessage(ChatMessage.VALID_USER, chatMessage.getUserFrom(), chatMessage.getUserTo(), ""));    
-                    break;
-                case ChatMessage.SEND_RSA:
-                case ChatMessage.SEND_RND:
-                case ChatMessage.SEED_INI:
-                    simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserTo(), "/queue/messages", chatMessage);
-                    break;                                
-                default: 
-                    simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserTo(), "/queue/messages", chatMessage);
-                    simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserFrom(), "/queue/messages", 
-                        new ChatMessage(ChatMessage.COMPLETED, chatMessage.getUserFrom(), chatMessage.getUserTo(), ""));    
-                    break;
-            }            
-        }else{
+            if(!chatRepository.chatExists(chatMessage.getUserFrom(), chatMessage.getUserTo())){
+                System.out.println("Chat save");
+                chatRepository.save(chat);
+            }        
+            if(userRepository.findByUsername(chatMessage.getUserTo()).get().isSignin()){
+                
+                System.out.println("Chat send");
+                switch(chatMessage.getType()){
+                    case ChatMessage.VALID_USER:
+                        simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserFrom(), "/queue/messages", 
+                        new ChatMessage(ChatMessage.VALID_USER, chatMessage.getUserFrom(), chatMessage.getUserTo(), ""));    
+                        break;
+                    case ChatMessage.SEND_RSA:
+                    case ChatMessage.SEND_RND:
+                    case ChatMessage.SEED_INI:
+                        simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserTo(), "/queue/messages", chatMessage);
+                        break;                                
+                    default: 
+                        simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserTo(), "/queue/messages", chatMessage);
+                        simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserFrom(), "/queue/messages", 
+                            new ChatMessage(ChatMessage.COMPLETED, chatMessage.getUserFrom(), chatMessage.getUserTo(), ""));    
+                        break;
+                }            
+            }else{
+                simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserFrom(), "/queue/messages", 
+                new ChatMessage(ChatMessage.ERROR, chatMessage.getUserFrom(), chatMessage.getUserTo(), "User not connected"));
+            }
+        }catch(NoSuchElementException e){
             simpMessagingTemplate.convertAndSendToUser(chatMessage.getUserFrom(), "/queue/messages", 
-            new ChatMessage(ChatMessage.ERROR, chatMessage.getUserFrom(), chatMessage.getUserTo(), "User not connected"));
+            new ChatMessage(ChatMessage.ERROR, chatMessage.getUserFrom(), chatMessage.getUserTo(), "User not exists"));
         }
     }
 }
